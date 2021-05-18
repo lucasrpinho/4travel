a854de8c289d30c73fc9b8988e3c10ab6be356b5
Lucas <lucasramospinho@gmail.com>
2021-05-18 15:56:52 -0300

    commit

---
+++ app/src/main/java/com/example/a4travel/activities/passeiosRecreio.java
@@ -0,0 +1,16 @@
+package com.example.a4travel.activities;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.a4travel.R;
+
+public class passeiosRecreio extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_passeios_recreio);
+    }
+}
